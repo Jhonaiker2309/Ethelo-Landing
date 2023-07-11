bb1c1b5e03ff67b453db17159a0d76a9e8e2fc1f
Jhonaiker2309 <blancojhonaiker@gmail.com>
2023-07-10 23:30:40 -0400

    Agregado a github page

--- src/views/footer-section/index.js
+++ src/views/footer-section/index.js
@@ -4,6 +4,9 @@ import ethelo from "../../images/ethelo.png";
 import twitter from "../../images/twitter.svg";
 import instagram from "../../images/instagram.svg";
 import facebook from "../../images/facebook.svg";
+import footer_icon_1 from "../../images/icon_time.png";
+import footer_icon_2 from "../../images/icon_contract.svg";
+import footer_icon_3 from "../../images/icon_win.png";
 
 export default function FooterSection() {
   return (
@@ -13,39 +16,60 @@ export default function FooterSection() {
           Why Ethelo?
         </Text>
         <SimpleGrid columns={{ sm: 1, lg: 3 }} spacing={[6, 10]}>
-          <Box color="white">
-            <Text fontSize={["xl", "2xl"]} fontWeight={600}>
-              Save time
-            </Text>
-            <Text fontSize={["md", "lg"]} fontWeight={300}>
-              Ethelo comes ready to let you <br />
-              seamlessly engage broad groups <br />
-              of stakeholders and quickly <br />
-              understand their priorities
-            </Text>
-          </Box>
-          <Box color="white">
-            <Text fontSize={["xl", "2xl"]} fontWeight={600}>
-              Find Agreement
-            </Text>
-            <Text fontSize={["md", "lg"]} fontWeight={300}>
-              Discover common ground in <br />
-              contentious, polarizing situations <br />
-              with Ethelo's patented scenario <br />
-              analysis technology
-            </Text>
-          </Box>
-          <Box color="white">
-            <Text fontSize={["xl", "2xl"]} fontWeight={600}>
-              Make better decisions
-            </Text>
-            <Text fontSize={["md", "lg"]} fontWeight={300}>
-              Create a well-articulated action <br />
-              plan based on input from a <br />
-              representative sample of your<br />
-              unique community
-            </Text>
-          </Box>
+          <Grid templateColumns='repeat(6, 1fr)'>
+            <GridItem colSpan={1}>
+              <Image src={footer_icon_1} w="70%" />
+            </GridItem>
+            <GridItem colSpan={5}>
+              <Box color="white" textAlign="left">
+                <Text fontSize={["xl", "2xl"]} fontWeight={600}>
+                  Save time
+                </Text>
+                <Text fontSize={["md", "lg"]} fontWeight={400}>
+                  Ethelo comes ready to let you <br />
+                  seamlessly engage broad groups <br />
+                  of stakeholders and quickly <br />
+                  understand their priorities
+                </Text>
+              </Box>
+            </GridItem>
+          </Grid>
+          <Grid templateColumns='repeat(6, 1fr)'>
+            <GridItem colSpan={1}>
+              <Image src={footer_icon_2} w="70%" />
+            </GridItem>
+            <GridItem colSpan={5}>
+              <Box color="white" textAlign="left">
+                <Text fontSize={["xl", "2xl"]} fontWeight={600}>
+                  Find Agreement
+                </Text>
+                <Text fontSize={["md", "lg"]} fontWeight={400}>
+                  Discover common ground in <br />
+                  contentious, polarizing situations <br />
+                  with Ethelo's patented scenario <br />
+                  analysis technology
+                </Text>
+              </Box>
+            </GridItem>
+          </Grid>
+          <Grid templateColumns='repeat(6, 1fr)'>
+            <GridItem colSpan={1}>
+              <Image src={footer_icon_3} w="70%" />
+            </GridItem>  
+            <GridItem colSpan={5}>        
+            <Box color="white" textAlign="left">
+              <Text fontSize={["xl", "2xl"]} fontWeight={600} textAlign="left">
+                Make better decisions
+              </Text>
+              <Text fontSize={["md", "lg"]} fontWeight={400}>
+                Create a well-articulated action <br />
+                plan based on input from a <br />
+                representative sample of your<br />
+                unique community
+              </Text>
+            </Box>
+            </GridItem>
+          </Grid>
         </SimpleGrid>
         <Center borderTop="2px" borderColor="white" mt={10}>
           <Text fontSize={["3xl", "4xl", "5xl"]} fontWeight={600} color="white" mt={20} mb={10}>
@@ -67,7 +91,7 @@ export default function FooterSection() {
       </Box>
       <Box>
         <Box h="13vh" py={5} px={20} w="100%">
-          <Grid templateColumns='repeat(5, 1fr)' gap={10} >
+          <Grid templateColumns='repeat(5, 1fr)' gap={10}>
             <GridItem colSpan={2}>
               <Flex align="center">
                 <Image src={ethelo} width="20%" />
